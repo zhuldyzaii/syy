f9dc9f30f7b433a574357392c7cbdef58cf3c36c
Aliya <Zhangirlhanaliya@mail.ru>
2025-04-06 22:04:14 +0500

    менююю істелінбей тұр

---
+++ app/src/main/java/com/example/syy/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.syy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Бос конструктор
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
+
--- app/src/main/java/com/example/syy/MainActivity.java
+++ app/src/main/java/com/example/syy/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.syy;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar-ды орнату
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Tazasy");
 
+        // Әдепкі фрагментті жүктеу
+        loadFragment(new HomeFragment());
+    }
+
+    // Менюны жүктеу (оң жақ жоғарғы бұрыштағы 3 нүкте)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+
+    // Меню элементі басылғанда әрекет жасау
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                loadFragment(new HomeFragment());
+                return true;
+            case R.id.nav_account:
+                loadFragment(new AccountFragment());
+                return true;
+            case R.id.nav_settings:
+                loadFragment(new SettingsFragment());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
