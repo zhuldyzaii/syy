d1d87342bda7139218da754afef82b18f4cf539e
Dilnaz <dilnaz_utegenova@mail.ru>
2025-04-08 09:45:11 +0500

    жасап тур

--- app/src/main/java/com/example/syy/MainActivity.java
+++ app/src/main/java/com/example/syy/MainActivity.java
@@ -3,54 +3,64 @@ package com.example.syy;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation; // 👈 Төменгі навигацияны анықтау
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Toolbar-ды орнату
+        // Toolbar орнату
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Tazasy");
 
-        // Әдепкі фрагментті жүктеу
+        // Төменгі навигацияны табу
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        // Навигация элементтеріне әрекет қосу
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.nav_account) {
+                selectedFragment = new AccountFragment();
+            } else if (id == R.id.nav_settings) {
+                selectedFragment = new SettingsFragment();
+            }
+
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+                return true;
+            }
+
+            return false;
+        });
+
+        // Әдепкі фрагмент жүктеу
         loadFragment(new HomeFragment());
     }
 
-    // Менюны жүктеу (оң жақ жоғарғы бұрыштағы 3 нүкте)
+    // Оң жақтағы 3 нүктелі меню (қаласаң алып тастауға болады)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
-
-    // Меню элементі басылғанда әрекет жасау
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_home:
-                loadFragment(new HomeFragment());
-                return true;
-            case R.id.nav_account:
-                loadFragment(new AccountFragment());
-                return true;
-            case R.id.nav_settings:
-                loadFragment(new SettingsFragment());
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-
+    // Фрагментті жүктеу функциясы
     private void loadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
